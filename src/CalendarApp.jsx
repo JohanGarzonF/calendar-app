f7c2591416643fe25f5017195a00cd036f2940eb
JohanGarzonF <johandanilogarzon@gmail.com>
2024-10-15 14:53:48 -0500

    Se añade redux y redux toolkit para manejo del estado global

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import { AppRouter } from './router'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 export const CalendarApp = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
   )
 }
